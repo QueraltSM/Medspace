15d5461a713c37128fe48ff79943293176d01944
QueraltSosaMompel <queraltsosa@hotmail.com>
2020-08-17 08:47:19 +0100

    First commit

---
+++ Medspace/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Medspace
+//
+//  Created by Queralt Sosa Mompel on 16/8/20.
+//  Copyright © 2020 Queralt Sosa Mompel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
